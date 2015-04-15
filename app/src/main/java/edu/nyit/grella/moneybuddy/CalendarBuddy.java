1a9e20c8336480e5670fc2d0f6d4f91b86cbafb2
GIO <grella@nyit.edu>
2015-04-14 20:42:18 -0400

    Selectable dates added to calendar

--- app/src/main/java/edu/nyit/grella/moneybuddy/CalendarBuddy.java
+++ app/src/main/java/edu/nyit/grella/moneybuddy/CalendarBuddy.java
@@ -1,57 +1,233 @@
 package edu.nyit.grella.moneybuddy;
 
-/**
- * Created by grella on 3/13/15.
- */
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidListener;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
+@SuppressLint("SimpleDateFormat")
+public class CalendarBuddy extends FragmentActivity {
+    private boolean undo = false;
+    private CaldroidFragment caldroidFragment;
+    private CaldroidFragment dialogCaldroidFragment;
 
-public class CalendarBuddy extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        CaldroidFragment caldroidFragment = new CaldroidFragment();
-        Bundle args = new Bundle();
-        Calendar cal = Calendar.getInstance();
-        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
-        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
-        caldroidFragment.setArguments(args);
+        final SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy");
 
+        caldroidFragment = new CaldroidFragment();
+
+        // If Activity is created after rotation
+        if (savedInstanceState != null) {
+            caldroidFragment.restoreStatesFromKey(savedInstanceState,
+                    "CALDROID_SAVED_STATE");
+        }
+        // If activity is created from fresh
+        else {
+            Bundle args = new Bundle();
+            Calendar cal = Calendar.getInstance();
+            args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+            args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+            args.putBoolean(CaldroidFragment.ENABLE_SWIPE, true);
+            args.putBoolean(CaldroidFragment.SIX_WEEKS_IN_CALENDAR, true);
+            caldroidFragment.setArguments(args);
+        }
+
+        // Attach to the activity
         FragmentTransaction t = getSupportFragmentManager().beginTransaction();
         t.replace(R.id.calendar1, caldroidFragment);
         t.commit();
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_calendar, menu);
-        return true;
+
+        // Setup listener
+        final CaldroidListener listener = new CaldroidListener() {
+
+            @Override
+            public void onSelectDate(Date date, View view) {
+                Toast.makeText(getApplicationContext(), formatter.format(date),
+                        Toast.LENGTH_SHORT).show();
+                Calendar cal = Calendar.getInstance();
+
+                // Min date is last 7 days
+                cal.add(Calendar.DATE, Integer.valueOf(formatter.format(date).substring(0,1)));
+                Date blueDate = cal.getTime();
+                caldroidFragment.setSelectedDates(date,date);
+                caldroidFragment.refreshView();
+            }
+
+            @Override
+            public void onChangeMonth(int month, int year) {
+                String text = "month: " + month + " year: " + year;
+                Toast.makeText(getApplicationContext(), text,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onLongClickDate(Date date, View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Long click " + formatter.format(date),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        };
+
+        // Setup Caldroid
+        caldroidFragment.setCaldroidListener(listener);
+
+        final TextView textView = (TextView) findViewById(R.id.textview);
+
+        final Button customizeButton = (Button) findViewById(R.id.customize_button);
+
+        // Customize the calendar
+        customizeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (undo) {
+                    customizeButton.setText(getString(R.string.customize));
+                    textView.setText("");
+
+                    // Reset calendar
+                    caldroidFragment.clearDisableDates();
+                    caldroidFragment.clearSelectedDates();
+                    caldroidFragment.setMinDate(null);
+                    caldroidFragment.setMaxDate(null);
+                    caldroidFragment.setShowNavigationArrows(true);
+                    caldroidFragment.setEnableSwipe(true);
+                    caldroidFragment.refreshView();
+                    undo = false;
+                    return;
+                }
+
+                // Else
+                undo = true;
+                customizeButton.setText(getString(R.string.undo));
+                Calendar cal = Calendar.getInstance();
+
+                // Min date is last 7 days
+                cal.add(Calendar.DATE, -7);
+                Date minDate = cal.getTime();
+
+                // Max date is next 7 days
+                cal = Calendar.getInstance();
+                cal.add(Calendar.DATE, 14);
+                Date maxDate = cal.getTime();
+
+                // Set selected dates
+                // From Date
+                cal = Calendar.getInstance();
+                cal.add(Calendar.DATE, 2);
+                Date fromDate = cal.getTime();
+
+                // To Date
+                cal = Calendar.getInstance();
+                cal.add(Calendar.DATE, 3);
+                Date toDate = cal.getTime();
+
+                // Set disabled dates
+                ArrayList<Date> disabledDates = new ArrayList<Date>();
+                for (int i = 5; i < 8; i++) {
+                    cal = Calendar.getInstance();
+                    cal.add(Calendar.DATE, i);
+                    disabledDates.add(cal.getTime());
+                }
+
+                // Customize
+                caldroidFragment.setMinDate(minDate);
+                caldroidFragment.setMaxDate(maxDate);
+                caldroidFragment.setDisableDates(disabledDates);
+                caldroidFragment.setSelectedDates(fromDate, toDate);
+                caldroidFragment.setShowNavigationArrows(false);
+                caldroidFragment.setEnableSwipe(false);
+
+                caldroidFragment.refreshView();
+
+                // Move to date
+                // cal = Calendar.getInstance();
+                // cal.add(Calendar.MONTH, 12);
+                // caldroidFragment.moveToDate(cal.getTime());
+
+                String text = "Today: " + formatter.format(new Date()) + "\n";
+                text += "Min Date: " + formatter.format(minDate) + "\n";
+                text += "Max Date: " + formatter.format(maxDate) + "\n";
+                text += "Select From Date: " + formatter.format(fromDate)
+                        + "\n";
+                text += "Select To Date: " + formatter.format(toDate) + "\n";
+                for (Date date : disabledDates) {
+                    text += "Disabled Date: " + formatter.format(date) + "\n";
+                }
+
+                textView.setText(text);
+            }
+        });
+
+        Button showDialogButton = (Button) findViewById(R.id.show_dialog_button);
+
+        final Bundle state = savedInstanceState;
+        showDialogButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Setup caldroid to use as dialog
+                dialogCaldroidFragment = new CaldroidFragment();
+                dialogCaldroidFragment.setCaldroidListener(listener);
+
+                // If activity is recovered from rotation
+                final String dialogTag = "CALDROID_DIALOG_FRAGMENT";
+                if (state != null) {
+                    dialogCaldroidFragment.restoreDialogStatesFromKey(
+                            getSupportFragmentManager(), state,
+                            "DIALOG_CALDROID_SAVED_STATE", dialogTag);
+                    Bundle args = dialogCaldroidFragment.getArguments();
+                    if (args == null) {
+                        args = new Bundle();
+                        dialogCaldroidFragment.setArguments(args);
+                    }
+                } else {
+                    // Setup arguments
+                    Bundle bundle = new Bundle();
+                    // Setup dialogTitle
+                    dialogCaldroidFragment.setArguments(bundle);
+                }
+
+                dialogCaldroidFragment.show(getSupportFragmentManager(),
+                        dialogTag);
+            }
+        });
     }
 
+    /**
+     * Save current states of the Caldroid here
+     */
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onSaveInstanceState(Bundle outState) {
+        // TODO Auto-generated method stub
+        super.onSaveInstanceState(outState);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (caldroidFragment != null) {
+            caldroidFragment.saveStatesToKey(outState, "CALDROID_SAVED_STATE");
         }
 
-        return super.onOptionsItemSelected(item);
+        if (dialogCaldroidFragment != null) {
+            dialogCaldroidFragment.saveStatesToKey(outState,
+                    "DIALOG_CALDROID_SAVED_STATE");
+        }
     }
+
 }
