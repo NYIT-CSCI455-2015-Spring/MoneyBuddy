69150f7acad54775d1380ffb9a3698f61e04f548
Anthony <abertoli@nyit.edu>
2015-04-24 11:36:18 -0400

    graph

--- app/src/main/java/edu/nyit/grella/moneybuddy/DailyExpenses.java
+++ app/src/main/java/edu/nyit/grella/moneybuddy/DailyExpenses.java
@@ -2,6 +2,7 @@ package edu.nyit.grella.moneybuddy;
 
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -30,24 +31,27 @@ public class DailyExpenses extends ListActivity {
     EditText itemEditText;
     EditText costEditText;
 
+    protected static String cost;
+    protected static ArrayList<String> costArray = new ArrayList();
+
     ListView listView;
     ArrayAdapter adapter;
     protected static ArrayList<String> arrayList = new ArrayList();
 
     // Creating the date
-    String date = new SimpleDateFormat("MM/dd/yyyy").format(new Date());
+    protected static String date = new SimpleDateFormat("MM/dd/yyyy").format(new Date());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_expenses);
 
-        // 1. Access the TextViews defined in layout XML
+        // Access the TextViews defined in layout XML
         // and then set its text
         itemTextView = (TextView) findViewById(R.id.item_textView);
         itemTextView.setText("Items");
 
-        // 2. Access the Buttons defined in layout XML
+        // Access the Buttons defined in layout XML
         // and listen for it here
         addButton = (Button) findViewById(R.id.add_button);
         addButton.setOnClickListener(listener);
@@ -55,11 +59,11 @@ public class DailyExpenses extends ListActivity {
         deleteButton = (Button) findViewById(R.id.delete_button);
         deleteButton.setOnClickListener(listenerDel);
 
-        // 3. Access the EditTexts defined in layout XML
+        // Access the EditTexts defined in layout XML
         itemEditText = (EditText) findViewById(R.id.item_edittext);
         costEditText = (EditText) findViewById(R.id.cost_edittext);
 
-        // 4. Access the ListView
+        // Access the ListView
         listView = (ListView) findViewById(android.R.id.list);
 
         // Create ArrayAdapters for the ListView
@@ -67,9 +71,16 @@ public class DailyExpenses extends ListActivity {
                 android.R.layout.simple_list_item_multiple_choice,
                 arrayList);
 
+        // LoadCostArray();
+
         // Set the ListViews to use the ArrayAdapter
         listView.setAdapter(adapter);
         LoadPreferences();
+    }
+
+    public void GraphButton (View v) {
+        Button ViewGraph = (Button) v;
+        startActivity(new Intent(getApplicationContext(), SavedSpent.class));
     }
 
     @Override
@@ -102,11 +113,23 @@ public class DailyExpenses extends ListActivity {
             // and use in the TextViews
             itemTextView.setText("Item");
 
-            String row = itemEditText.getText().toString() + "          $" + costEditText.getText().toString() + "\n" + date;
+            // putting user input into an array
+            cost = costEditText.getText().toString();
+            if (cost.length() != 0) {
+                costArray.add(cost);
+            }
+
+            // setting user inputs to strings
+            cost = costEditText.getText().toString();
+            String item = itemEditText.getText().toString();
+
+            String row = item + "          $" + cost + "\n" + date;
 
             // Also add that value to the lists shown in the ListViews
-            arrayList.toString();
+            // arrayList.toString();
             arrayList.add(row);
+            costArray.add(cost);
+            SaveCostArray();
             SavePreferences();
             adapter.notifyDataSetChanged();
         }
@@ -123,15 +146,18 @@ public class DailyExpenses extends ListActivity {
             for (int i=itemCount-1; i >= 0; i--){
                 if (checkedItemPositions.get(i)) {
                     adapter.remove(arrayList.get(i));
+                    //adapter.remove(costArray.get(i));
                 }
             }
-            checkedItemPositions.clear();
+            checkedItemPositions.clear();               // deletes the data from the listview if it is checked
             SavePreferences();
             adapter.notifyDataSetChanged();
         }
     };
 
+    // saving the values entered into the textviews in sharedprefs
     protected void SavePreferences() {
+        // saving data in sharedprefs to be shown in listview
         SharedPreferences data = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = data.edit();
         editor.putInt("DE_Status_size", arrayList.size());
@@ -144,6 +170,7 @@ public class DailyExpenses extends ListActivity {
         editor.commit();
     }
 
+    // loading the values from sharedprefs into the listview
     protected void LoadPreferences() {
         SharedPreferences data = PreferenceManager.getDefaultSharedPreferences(this);
         arrayList.clear();
@@ -154,4 +181,30 @@ public class DailyExpenses extends ListActivity {
         }
         adapter.notifyDataSetChanged();
     }
+
+    protected void SaveCostArray() {
+        // saving the cost in an array using sharedprefs to be used for graphing
+        SharedPreferences cost_data = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor cost_editor = cost_data.edit();
+        cost_editor.putInt("CostArray_Status_size", costArray.size());
+
+        for (int i=0; i<arrayList.size(); i++){
+            costArray.remove("CostArray_Status_" + i);
+            cost_editor.putString("CostArray_Status_" + i, costArray.get(i));
+        }
+
+        cost_editor.commit();
+    }
+
+    /*
+    protected void LoadCostArray() {
+        SharedPreferences cost_data = PreferenceManager.getDefaultSharedPreferences(this);
+        costArray.clear();
+        int size = cost_data.getInt("CostArray_Status_size", 0);
+
+        for (int i=0; i<size; i++) {
+            costArray.add(cost_data.getString("CostArray_Status_" + i, null));
+        }
+    }
+    */
 }
--- app/src/main/java/edu/nyit/grella/moneybuddy/SavedSpent.java
+++ app/src/main/java/edu/nyit/grella/moneybuddy/SavedSpent.java
@@ -4,34 +4,114 @@ package edu.nyit.grella.moneybuddy;
  * Created by grella on 3/9/15.
  */
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.ArrayList;
 
 public class SavedSpent extends ActionBarActivity {
+
+    public static ArrayList yvalue;
+    public static ArrayList<Integer> yvalues;
+    //public static int Yvalue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_savedspent);
         GraphView graph = (GraphView) findViewById(R.id.graph);
+
+        yvalue = DailyExpenses.costArray;
+        yvalues = getIntegerArray(yvalue);
+
+        // check to see if cost of item was added during a specified month
+        // then add the ones in the same month - use a for loop
+
+        /*
+        for (Integer i: yvalues) {
+            Yvalue = yvalues.get(i);
+        }
+        */
+
+
+        // getting the month
+        String date = DailyExpenses.date;
+        String[] split = date.split("/");
+        String monthstr = split[0];
+        int month = Integer.parseInt(monthstr);
+        System.out.println(month);
+
+        // DataPoint[] data = new DataPoint[5];    // DailyExpenses.arrayList
+
+        graph.getViewport().setScalable(true);
+        graph.getViewport().setScrollable(true);
+
+        // use static labels for horizontal label to show months
+        final StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setHorizontalLabels(new String[] {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"});
+        //staticLabelsFormatter.setVerticalLabels(new String[]{"$0", "$250", "$500", "$750", "$1000", "$1250", "$1500"});
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+        // LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(data);
+
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]
                 {
-                        new DataPoint(0, 1),
-                        new DataPoint(1, 5),
-                        new DataPoint(2, 3),
-                        new DataPoint(3, 2),
-                        new DataPoint(4, 6)
+                        new DataPoint(0, yvalues.get(0)),
+                        new DataPoint(1, yvalues.get(1)),
+                        new DataPoint(2, yvalues.get(2)),
+                        new DataPoint(3, yvalues.get(3)),
+                        new DataPoint(4, yvalues.get(4))
                 });
 
+
+        series.setTitle("Spending per Month");
+        series.setColor(Color.GREEN);
+        series.setDrawDataPoints(true);
+        series.setDataPointsRadius(5);
+        series.setThickness(5);
         graph.addSeries(series);
     }
+
+    private ArrayList<Integer> getIntegerArray(ArrayList<String> stringArray) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(String stringValue : stringArray) {
+            try {
+                //Convert String to Integer, and store it into integer array list.
+                result.add(Integer.parseInt(stringValue));
+            } catch(NumberFormatException nfe) {
+                //System.out.println("Could not parse " + nfe);
+                Log.w("NumberFormat", "Parsing failed! " + stringValue + " can not be an integer");
+            }
+        }
+        return result;
+    }
+
+    /*
+    public Integer getYValue(){
+        for (Integer i: yvalues) {
+            return i;
+        }
+        return -1;
+    }
+    */
+
+    /*
+    public static int getMonth(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(DailyExpenses.date);
+        return cal.get(Calendar.MONTH);
+    }
+    */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
